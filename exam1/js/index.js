f9ff4be26776399e02552f946757351d4c3a25b5
hyemyn2 <hyemyn2@gmail.com>
2020-09-23 17:58:08 +0900

    3

--- exam1/js/index.js
+++ exam1/js/index.js
@@ -1,57 +1,5 @@
 function init(){
 
-var burger = document.querySelector('.burger');
-var menu = document.querySelector('.menu');
-
-var viewTag = document.querySelector('.txt a');
-var pop = document.querySelector('.pop');
-var popBtn = document.querySelector('.pop .btn')
-
-
-// burger
-// if(menu.className=='active'){
-
-// }
-burger.addEventListener('click',showMenu);
-
-function showMenu(){
-    if(burger.classList!='active'){
-        burger.classList.add('active');
-        menu.classList.add('active');
-    } else if(burger.classList=='active'){
-        burger.classList.remove('active');
-        menu.classList.remove('active');
-    }
-
-
-}
-// menu.classList.add('active');
-
-// function showNav(){
-//     menu.classList.remove('active');
-//     burger.addEventListener('click',off);
-// }
-
-// function off(){
-//     menu.style = 'opacity:0';
-// }
-
-
-// pop
-viewTag.addEventListener('click', popUp);
-popBtn.addEventListener('click',popOff);
-function popUp(){
-    pop.classList.add('active');
-}
-function popOff(){
-    pop.classList.remove('active');
-}
-s
-
-
-// ---------------------------------------------------------------- jquery
-/*
-
 $('.burger').on('click',showNav);
 
 function showNav(){
@@ -61,7 +9,8 @@ function showNav(){
 
 $('.txt a').on('click',popUp);
 
-function popUp(){
+function popUp(e){
+    e.preventDefault();
     $('.pop').addClass('active');
     $('.pop .btn').on('click',popOff);
 }
@@ -69,9 +18,6 @@ function popUp(){
 function popOff(){
     $('.pop').removeClass('active');
 }
-
-*/
-
 
 }
 
