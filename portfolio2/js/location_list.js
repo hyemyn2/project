911db2371b80080700dff802859f267975143329
hyemyn2 <hyemyn2@gmail.com>
2020-10-26 09:55:58 +0900

    10/26

---
+++ portfolio2/js/location_list.js
@@ -0,0 +1,60 @@
+$(function(){
+
+    $.ajax({        
+        url:'/json/location_list.json',
+        type:'GET',
+        success:function(data){
+
+            var list = document.querySelector('.city');
+            var tagName='', title, intTxt, intImg;
+
+
+            data.location.forEach(function(i){
+                title = i.country;
+                intTxt = i.introText;
+                intImg = i.introImg;
+
+                tagName+= "<li>";
+                tagName+= "<div class='txt'>";
+                tagName+= "<p>"+title+"</p>";
+                tagName+= "<span>"+intTxt+"</span>";
+                tagName+= "</div>";
+                tagName+= "<div class='pic'>";
+                tagName+= "<img src='"+i.introImg+"' alt=''>";
+                tagName+= "</div>";
+                tagName+= "</li>";
+            });
+            $('.city ul').append(tagName);
+
+
+            // list click 이벤트
+            var city = document.querySelectorAll('.city li');
+            city.forEach(function(i){
+                i.addEventListener('click',function(){
+                    // console.log(window.location.pathname);
+                    window.location.pathname = "/location/location_detail.html";
+                    var str = i.childNodes[1].childNodes[0].src;
+
+                    var strStart = str.lastIndexOf('/')+1;
+                    var strEnd = str.lastIndexOf('.');
+                    var cityName = str.slice(strStart, strEnd);
+
+                    console.log(cityName)
+                    localStorage.city = cityName;
+
+                });
+            });
+
+
+
+
+        }
+    })
+
+
+
+
+
+
+
+});
---
+++ project-master/project-master/portfolio2/js/common.js
@@ -0,0 +1,114 @@
+
+$(function(){
+
+    $('header').load('/inc_head_foot.html header .tie',head);
+    $('footer').load('/inc_head_foot.html footer .foot');
+    
+
+
+// =========================main
+
+// var delta, scrollNum=0;
+    // $('html').css({overflow:'hidden'});
+    // $('html').on('mousewheel',function(e){
+    //     goDown(e);
+    // });
+    // console.log(e.originalEvent.wheelDelta);
+
+
+    // function goDown(e){
+    //     delta = e.originalEvent.wheelDeltaY;
+    //     // console.log(delta);
+
+    //     if(delta<0){
+    //         scrollNum++;
+    //     }else if(delta>0){
+    //         scrollNum--;
+    //     }
+
+    //     console.log(scrollNum);
+
+    //     $('main').css({transform:"translateY(-"+120*scrollNum+"px)"});
+
+    // }
+
+
+    $('html').scrollTop(0);
+    $('html').css({overflow:"hidden"});
+    $('html').on('mousewheel',function(e){
+        goDown(e);
+    });
+
+    var idx=0,plus=200;
+    var main = document.querySelector('main');
+    // var ft = document.querySelector('.foot');
+
+    function goDown(e){
+        var delta = e.originalEvent.wheelDeltaY;
+        var eleHeight = document.body.offsetHeight - window.innerHeight;
+        (delta<0) ? idx +=plus: idx-=plus;
+        
+        if(eleHeight <= idx){
+            idx = eleHeight;
+        }else if(idx < 0){
+            idx = 0;
+        }
+        $('main,footer').css({transform:"translateY(-"+idx+"px)"});
+
+    }
+
+    // --------------------- header
+
+    function head(){
+
+        var loca = document.querySelector('.location ul');
+        var tagName = "";
+        var menu = document.querySelectorAll('nav ul li a');
+        console.log(menu)
+        menu.forEach(function(i,k){
+
+
+            i.addEventListener('click',function(e){
+                e.preventDefault();
+                // tagName = "<li>"+i.childNodes[1].innerText+"</li>";
+                // loca.innerHTML += tagName;
+
+                localStorage.page = this.textContent;
+                location.href = this.href;
+            });
+        });
+
+        var page = localStorage.page;
+
+
+        tagName = "<li>></li><li>"+page+"</li>";
+        loca.innerHTML += tagName;
+
+        // console.log(page)
+
+        // 인재영업 페이지 이동
+        var menuCareer = document.querySelector('.go_career');
+        menuCareer.addEventListener('click',function(e){
+            e.preventDefault();
+            window.open("/career/career_main.html","_blank");
+        });
+
+        var bg = document.querySelector('.head .burger');
+        var bgNav = document.querySelector('.head nav')
+        bg.addEventListener('click',function(){
+            bg.classList.toggle('active');
+            bgNav.classList.toggle('active');
+        });
+    }
+
+
+
+
+
+});
+
+        // switch(page){
+        //     case '쿠팡 소개': ; break;
+        //     case '걸어온 길': ; break;
+        //     case '회사 위치': ; break;
+        // }
