c76b4f288f6d10195f67b73f3d2103a7f073f08a
hyemyn2 <hyemyn2@gmail.com>
2020-10-29 17:27:27 +0900

    10/29

---
+++ portfolio3/js/index.js
@@ -0,0 +1,8 @@
+function init(){
+    // var img = document.querySelectorAll('.con_shop li')
+    // console.log(img[1].offsetWidth);
+    // console.log(img[1].offsetHeight);
+    // 1084 & 530
+}
+
+window.addEventListener('DOMContentLoaded',init);
