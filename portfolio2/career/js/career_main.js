b772a888b3e8b26ab225872e18c44d6410787862
hyemyn2 <hyemyn2@gmail.com>
2020-11-27 18:55:56 +0900

    1127

--- portfolio2/career/js/career_main.js
+++ portfolio2/career/js/career_main.js
@@ -4,18 +4,18 @@ $(function(){
     var smallCon = document.querySelectorAll('main .small section');
 
     bigCon.addEventListener('click',function(){
-        window.location.pathname = "../apply/career_apply_jobs.html";
+        window.location.pathname = "project/portfolio2/career/apply/career_apply_jobs.html";
     });
 
     smallCon.forEach(function(i,k){
         smallCon[k].addEventListener('click',function(){
             console.log(k)
             if(k==0){
-                window.location.pathname = "portfolio2/career/apply/career_apply_delivery.html";
+                window.location.pathname = "project/portfolio2/career/apply/career_apply_delivery.html";
             }else if(k==1){
-                window.location.pathname = "portfolio2/career/career_family.html";
+                window.location.pathname = "project/portfolio2/career/career_family.html";
             }else if(k==2){
-                window.location.pathname = "portfolio2/index.html";
+                window.location.pathname = "project/portfolio2/index.html";
             }
 
         });
