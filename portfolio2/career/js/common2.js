a63db6f889e44cba66d993b51050b68c5243fa92
hyemyn2 <hyemyn2@gmail.com>
2020-11-27 16:31:01 +0900

    11/27 9

--- portfolio2/career/js/common2.js
+++ portfolio2/career/js/common2.js
@@ -1,8 +1,8 @@
 
 $(function(){
 
-    $('header').load('inc_head_foot.html header .tie_2',head);
-    $('footer').load('inc_head_foot.html footer .foot_2');
+    $('header').load('../inc_head_foot.html header .tie_2',head);
+    $('footer').load('../inc_head_foot.html footer .foot_2');
 
     function head(){
 
